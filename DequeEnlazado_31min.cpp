379414e179e5eb8d6cae4d44a18741ae58f59e6f
diegougu <128000056+diegougu@users.noreply.github.com>
2024-10-08 15:38:23 -0500

    Update and rename DequeEnlazado_1h11min.cpp to DequeEnlazado_31min.cpp

--- DequeEnlazado_31min.cpp
+++ DequeEnlazado_31min.cpp
@@ -6,7 +6,7 @@ struct arrnode {
 	const static int tam = 5;
 	T arr[tam];
 	arrnode<T>* next = nullptr;
-	arrnode<T>*prev = nullptr;
+	arrnode<T>* prev = nullptr;
 	arrnode(arrnode<T>* n = nullptr, arrnode<T>* p = nullptr) : next(n), prev(p) {}
 };
 
@@ -55,10 +55,8 @@ void LinkedDeque<T>::push_back(T v) {
 			*back_arr = v;
 			elementos++;
 		}
-
 	}
 }
-
 
 template <class T>
 void LinkedDeque<T>::push_front(T v) {
@@ -71,17 +69,16 @@ void LinkedDeque<T>::push_front(T v) {
 		elementos++;
 	}
 	else {
-		arrnode<T>* p = head;
-		while (p->prev != nullptr) {
-			p = p->prev;
+		arrnode<T>* f = head;
+		while (f->prev != nullptr) {
+			f = f->prev;
 		}
-
-		if (front_arr == p->arr) {
+		if (front_arr == f->arr) {
 			arrnode<T>* temp = new arrnode<T>;
-			p->prev = temp;
-			temp->next = p;
-			p = p->prev;
-			front_arr = p->arr + p->tam - 1;
+			temp->next = f;
+			f->prev = temp;
+			f = f->prev;
+			front_arr = f->arr + f->tam - 1;
 			*front_arr = v;
 			elementos++;
 		}
@@ -90,74 +87,67 @@ void LinkedDeque<T>::push_front(T v) {
 			*front_arr = v;
 			elementos++;
 		}
-
 	}
 }
 
 
 template <class T>
 void LinkedDeque<T>::pop_back() {
-	if (head == nullptr) {
+	if (head == nullptr || (back_arr == head->arr + (head->tam / 2) && front_arr != head->arr + (head->tam / 2))) {
 		return;
 	}
-	else if (back_arr == head->arr + (head->tam / 2)) {
-		return;
-	}
-	else if (back_arr == head->arr + (head->tam / 2) && front_arr == back_arr) {
+	else if (back_arr == head->arr + (head->tam / 2) && front_arr == head->arr + (head->tam / 2)) {
 		arrnode<T>* temp = head;
 		head = nullptr;
-		back_arr = nullptr;
 		front_arr = nullptr;
+		back_arr = nullptr;
+		elementos--;
 		delete temp;
 	}
 	else {
-		arrnode<T>* temp = head;
-		while (temp->next != nullptr) {
-			temp = temp->next;
+		arrnode<T>* u = head;
+		while (u->next != nullptr) {
+			u = u->next;
 		}
-
-		if (back_arr == temp->arr) {
-			arrnode<T>* n = temp->prev;
-			n->next = nullptr;
-			back_arr = n->arr + n->tam - 1;
-			elementos--;
+		if (back_arr == u->arr) {
+			arrnode<T>* temp = u;
+			u = u->prev;
+			back_arr = u->arr + u->tam - 1;
+			u->next = nullptr;
 			delete temp;
+			elementos--;
 		}
 		else {
 			back_arr--;
 			elementos--;
 		}
-
 	}
 }
 
-
-
 template <class T>
 void LinkedDeque<T>::pop_front() {
-	if (head == nullptr) {
+	if (head == nullptr || (front_arr == head->arr + (head->tam / 2) && back_arr != head->arr + (head->tam / 2))) {
 		return;
 	}
-	else if (front_arr == head->arr + (head->tam / 2)) {
-		return;
-	}
-	else if (front_arr == head->arr + (head->tam / 2) && back_arr == front_arr) {
+	else if (front_arr == head->arr + (head->tam / 2) && back_arr == head->arr + (head->tam / 2)) {
 		arrnode<T>* temp = head;
 		head = nullptr;
-		back_arr = nullptr;
 		front_arr = nullptr;
+		back_arr = nullptr;
+		elementos--;
 		delete temp;
 	}
 	else {
-		arrnode<T>* temp = head;
-		while (temp->prev != nullptr) {
-			temp = temp->prev;
+		arrnode<T>* f = head;
+		while (f->prev != nullptr) {
+			f = f->prev;
 		}
 
-		if (front_arr == temp->arr + temp->tam - 1) {
-			arrnode<T>* n = temp->next;
-			n->prev = nullptr;
-			front_arr = n->arr;
+		if (front_arr == f->arr + f->tam - 1) {
+			arrnode<T>* temp = f;
+			f = f->next;
+			front_arr = f->arr;
+			f->prev = nullptr;
 			elementos--;
 			delete temp;
 		}
@@ -165,26 +155,53 @@ void LinkedDeque<T>::pop_front() {
 			front_arr++;
 			elementos--;
 		}
+
 	}
 }
 
+template <class T>
+T& LinkedDeque<T>::operator[](int v) {
+	if (v >= elementos) {
+		static T a;
+		return a;
+	}
+	
+	arrnode<T>* f = head;
+	int c = 0;
+	while (f->prev != nullptr) {
+		f = f->prev;
+	}
+	T* i = front_arr;
+
+	while (f->next != nullptr) {
+		while (i <= f->arr + f->tam - 1 && i <= back_arr) {
+			if (c == v) {
+				return *i;
+			}
+			i++;
+			c++;
+		}
+		f = f->next;
+		i = f->arr;
+	}
+}
 
 template <class T>
 void LinkedDeque<T>::print() {
 	arrnode<T>* p_a = head;
-	while (p_a->prev != nullptr){
+	while (p_a->prev != nullptr) {
 		p_a = p_a->prev;
 	}
 
 	T* i = front_arr;
 	arrnode<T>* p = p_a;
-	
+
 	while (p != nullptr) {
-		cout << " [ ";
+		cout << "[ ";
 		while (i < p->arr + p->tam) {
 			cout << *i << " ";
 			if (i == back_arr) {
-				cout << " ] ";
+				cout << " ]->";
 				cout << endl;
 				return;
 			}
@@ -199,38 +216,10 @@ void LinkedDeque<T>::print() {
 
 }
 
-
-template <class T>
-T& LinkedDeque<T>::operator[](int v) {
-	if (v >= elementos) {
-		T r;
-		return r;
-	}
-	else {
-		arrnode<T>* p_a = head;
-		while (p_a->prev != nullptr) {
-			p_a = p_a->prev;
-		}
-
-		T* i = front_arr;
-		int c = 0;
-		while (c != v) {
-			if (i >= p_a->arr + p_a->tam - 1) {
-				p_a = p_a->next;
-				i = p_a->arr;
-			}
-			c++;
-			i++;
-		}
-		return *i;
-		
-	}
-}
-
 int main() {
 	LinkedDeque<int> deq;
-	int arr[5] = {1,2,3,4,5};
-	int arr2[5] = {6,7,8,9,10};
+	int arr[5] = { 1,2,3,4,5 };
+	int arr2[5] = { 6,7,8,9,10 };
 
 	for (int i = 0; i < 5; i++) {
 		deq.push_back(arr[i]);
